6c4dbdbb954dc8cc244259900d30ede0578ec878
s3b4st1i3n-G <sebgrge04@gmail.com>
2023-11-22 14:13:07 +0100

    fix: crud action add

--- dictionary/actions.go
+++ dictionary/actions.go
@@ -29,3 +29,30 @@ func (d *Dictionary) Add(word string, definition string) error {
 		return txn.Set([]byte(word), buffer.Bytes())
 	})
 }
+
+func (d *Dictionary) Get(word string) (Entry, error) {
+	var entry Entry
+	err := d.db.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte(word))
+		if err != nil {
+			return err
+		}
+
+		entry, err = getEntry(item)
+		return err
+	})
+	return entry, err
+}
+
+func getEntry(item *badger.Item) (Entry, error) {
+	var entry Entry
+	var buffer bytes.Buffer
+	err := item.Value(func(val []byte) error {
+		_, err := buffer.Write(val)
+		return err
+	})
+
+	dec := gob.NewDecoder(&buffer)
+	err = dec.Decode(&entry)
+	return entry, err
+}
--- main.go
+++ main.go
@@ -10,6 +10,10 @@ func main() {
 	d, err := dictionary.New("./badger")
 	handleErr(err)
 	defer d.Close()
+
+	d.Add("golang", "A wonderful language")
+	entry, _ := d.Get("golang")
+	fmt.Println(entry)
 }
 
 func handleErr(err error) {
