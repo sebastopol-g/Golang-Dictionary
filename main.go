591890ab5c15178b522d2d2caf9c7639e4023331
s3b4st1i3n-G <sebgrge04@gmail.com>
2023-11-22 16:17:36 +0100

    fix: add define and remove action

--- main.go
+++ main.go
@@ -2,24 +2,61 @@ package main
 
 import (
 	"dictionary/dictionary"
+	"flag"
 	"fmt"
 	"os"
 )
 
 func main() {
+
+	action := flag.String("action", "list", "Action to perform on the dictionary")
+
 	d, err := dictionary.New("./badger")
 	handleErr(err)
 	defer d.Close()
 
-	d.Add("python", "An interpreted language")
-	words, entries, _ := d.List()
-	for _, word := range words {
-		fmt.Println(entries[word])
+	flag.Parse()
+	switch *action {
+	case "list":
+		actionList(d)
+	case "add":
+		actionAdd(d, flag.Args())
+	case "define":
+		actionDefine(d, flag.Args())
+	case "delete":
+		actionDelete(d, flag.Args())
+	default:
+		fmt.Printf("Unknown action: %v\n", *action)
 	}
+}
 
-	d.Remove("python")
+func actionAdd(d *dictionary.Dictionary, args []string) {
+	word := args[0]
+	definition := args[1]
+	err := d.Add(word, definition)
+	handleErr(err)
+	fmt.Printf("'%v' added to the dictionary\n", word)
+}
 
-	words, entries, _ = d.List()
+func actionDefine(d *dictionary.Dictionary, args []string) {
+	word := args[0]
+	entry, err := d.Get(word)
+	definition := entry.Definition
+	handleErr(err)
+	fmt.Printf("Definition is :'%v'\n", definition)
+}
+
+func actionDelete(d *dictionary.Dictionary, args []string) {
+	word := args[0]
+	err := d.Remove(word)
+	handleErr(err)
+	fmt.Printf("'%v' deleted from to the dictionary\n", word)
+}
+
+func actionList(d *dictionary.Dictionary) {
+	words, entries, err := d.List()
+	handleErr(err)
+	fmt.Println("Dictionary content")
 	for _, word := range words {
 		fmt.Println(entries[word])
 	}
